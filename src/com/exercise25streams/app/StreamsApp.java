93cf4874a6fe2e06ac533cb4ae95ab060250e062
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-01-20 12:04:48 -0600

    Streams exercise

---
+++ src/com/exercise25streams/app/StreamsApp.java
@@ -0,0 +1,45 @@
+package com.exercise25streams.app;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+
+public class StreamsApp 
+{
+	public static void main(String[] args) 
+	{
+		File myFile = null;
+		FileReader myReader = null;
+		BufferedReader myBuffer = null;
+		String line = "";
+		
+		try 
+		{
+			myFile = new File("D:\\0\\a.txt");
+			myReader = new FileReader(myFile);
+			myBuffer  = new BufferedReader(myReader);
+			
+			while ((line = myBuffer.readLine()) != null)
+			{				
+				System.out.println(line);
+			}
+		}
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try {
+				myBuffer.close();
+				myReader = null;
+				myFile = null;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+}
